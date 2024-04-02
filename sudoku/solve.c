ab7507f79b7af45dc82aa51a8400f99838941144
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-04-02 16:52:34 +0200

    On renomme le fichier sudoku_solve.c en solve.c

---
+++ sudoku/solve.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sudoku_safe.h"
+
+int remplir_case(int valeur, int* mémoire){
+    valeur=rand()(%9);
+    for (int j =0 ; j<=8 ; j++){
+        if (mémoire[j]==0){
+            if (mémoire[valeur-1]==1){
+                remplir_case(valeur,mémoire);
+            }
+            else{
+                return valeur;
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+}
+
+
+bool sudoku_solve(int** grile){
+    int ligne = 0;
+    int colonne = 0;
+    int valeur=0;
+    for(int a=0; a<=9 ; a++){
+        for(int b=0 ; b<=9 ; b++){
+            if (grille[a][b]==0){
+                    while (grille[ligne][colonne]=!0){
+                    ligne=rand()%(9);
+                    colonne=rand()%(9);
+                }
+                int mémoire[9];
+                for (int i=0;  i<=8 ; i++){
+                    if (safe(grille, ligne,colonne,i+1)){
+                        mémoire[i]=0;
+                    };
+                    else{
+                        mémoire[i]=1;
+                    }
+                }
+                valeur = remplir_case(valeur, mémoire);
+                if (valeur!=0){
+                    grille[ligne][colonne]=valeur;
+                    sudoku_solve(grille);
+                }
+                else {
+                    return false
+                }
+            }
+        else {
+            return true
+            }
+        }
+    }
+}
+
+
