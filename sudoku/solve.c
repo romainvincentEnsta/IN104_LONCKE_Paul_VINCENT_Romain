a4616c95cfe32651e6a43d30d1275474d3404797
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-04-04 19:42:23 +0200

    Modification

--- sudoku/solve.c
+++ sudoku/solve.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "safe.h"
+#include "solve.h"
+#include "afficher_sudoku.h"
 
 int remplir_case(int valeur, int* mémoire){
-    valeur=rand()(%9);
+    valeur=rand()%(9);
     for (int j =0 ; j<=8 ; j++){
         if (mémoire[j]==0){
             if (mémoire[valeur-1]==1){
@@ -21,14 +23,14 @@ int remplir_case(int valeur, int* mémoire){
 }
 
 
-bool sudoku_solve(int** grile){
+bool sudoku_solve(int** grille){
     int ligne = 0;
     int colonne = 0;
     int valeur=0;
     for(int a=0; a<=9 ; a++){
         for(int b=0 ; b<=9 ; b++){
             if (grille[a][b]==0){
-                    while (grille[ligne][colonne]=!0){
+                    while (grille[ligne][colonne]!=0){
                     ligne=rand()%(9);
                     colonne=rand()%(9);
                 }
@@ -36,7 +38,7 @@ bool sudoku_solve(int** grile){
                 for (int i=0;  i<=8 ; i++){
                     if (safe(grille, ligne,colonne,i+1)){
                         mémoire[i]=0;
-                    };
+                    }
                     else{
                         mémoire[i]=1;
                     }
@@ -47,11 +49,11 @@ bool sudoku_solve(int** grile){
                     sudoku_solve(grille);
                 }
                 else {
-                    return false
+                    return false;
                 }
             }
         else {
-            return true
+            return true;
             }
         }
     }
