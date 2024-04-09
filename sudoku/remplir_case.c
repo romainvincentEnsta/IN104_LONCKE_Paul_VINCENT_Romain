81eeffb9894baca6f321de48c33dd149cd5a3b09
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-04-09 16:29:29 +0200

    Ajout

---
+++ sudoku/remplir_case.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "remplir_case.h"
+
+int remplir_case(int valeur, int* mémoire){
+    valeur=rand()%(9)+1;
+    for (int j =0 ; j<=8 ; j++){
+        if (mémoire[j]==0){
+            if (mémoire[valeur-1]==1){
+                remplir_case(valeur,mémoire);
+                return 0;
+            }
+            else{
+                return valeur;
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+    return valeur;
+}
+
+int main(){
+    return 0;
+}
---
+++ sudoku/remplir_case.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+int remplir_case(int valeur, int* mémoire);
