e45b07da83fe97ad5d7a21f1b9df0f06c7ba6ead
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-04-16 15:12:10 +0200

    Modif

---
+++ morpion/includes/est_plein.h
@@ -0,0 +1,6 @@
+#ifndef EST_PLEIN_H
+#define EST_PLEIN_H
+
+int est_plein(int* grille);
+
+#endif
---
+++ morpion/includes/placer.h
@@ -0,0 +1,6 @@
+#ifndef PLACER_H
+#define PLACER_H
+
+int placer (int* grille, int chiffre, int joueur);
+
+#endif
