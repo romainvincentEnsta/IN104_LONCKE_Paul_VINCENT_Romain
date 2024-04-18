fb2540cc7bc9a5da264a71db81781d2c1cf48d15
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-04-18 18:11:50 +0200

    grille_pleine.c

--- sudoku/grille_pleine.c
+++ sudoku/grille_pleine.c
@@ -2,8 +2,8 @@
 #include "grille_pleine.h"
 
 int  grille_pleine(int** grille){
-    for (int i==0 ; i<9 ; i++){
-        for(int j==0 ; j<9 ; j++){
+    for (int i=0 ; i<9 ; i++){
+        for(int j=0 ; j<9 ; j++){
             if (grille[i][j]==0){
                 return 1;
             }
