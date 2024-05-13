4759ac23bd3a78433ea7f233c817d3bed59fc932
romainvincentEnsta <romain.vincent@ensta-paris.fr>
2024-05-13 19:48:31 +0200

    sudoku/

--- sudoku/grille_pleine.c
+++ sudoku/grille_pleine.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "grille_pleine.h"
 
-int  grille_pleine(int** grille){
+int  grille_pleine(int** grille){     //La fonction parcourt la grille qui a précédemment été initialisée à zéro et renvoit 0 si tous les coefficients sont non nuls et 1 sinon
     for (int i=0 ; i<9 ; i++){
         for(int j=0 ; j<9 ; j++){
-            if (grille[i][j]==0){
+            if (grille[i][j]==0){  //On teste si le coefficient vaut 0 et n'a donc pas encore été remplit
                 return 1;
             }
         }
