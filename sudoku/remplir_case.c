5cb670ae64286d628d15fffacfab50ed52027699
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-05-14 15:18:13 +0200

    Modifs

--- sudoku/remplir_case.c
+++ sudoku/remplir_case.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "remplir_case.h"
+#include "tableau_plein.h"
 #include <time.h>
 
 int remplir_case(int valeur, int* mémoire){
-<<<<<<< HEAD
-    if (mémoire[valeur-1]==1){
-        return 1;
-    }
-    else {
-        return 0;
-=======
     srand(time(0));  //On veut des nombres aléatoires différents
     valeur=rand()%(9)+1;  //On initialise valeur
     if (tableau_plein(mémoire)){     //Si mémoire n'est pas déjà plein
@@ -20,7 +14,6 @@ int remplir_case(int valeur, int* mémoire){
         else{
             return valeur;
         }
->>>>>>> a625a7a3da9adc9b270970c4dc5716b93cd3c8e1
     }
 }
 
