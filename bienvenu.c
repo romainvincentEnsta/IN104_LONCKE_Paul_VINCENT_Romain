4203bdf304649fb695fea6d6886fa10fc7443cb9
NFSH1206 <paul.loncke@ensta-paris.fr>
2024-05-21 13:51:08 +0200

    Correction

--- bienvenu.c
+++ bienvenu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    printf(" BIENVENUE AUX 4 JEUX\n Pour commencer à jouer, veuillez éxécuter la commande suivante\n bash execution.sh\n")
+    printf(" BIENVENUE AUX 4 JEUX\n Pour commencer à jouer, veuillez éxécuter la commande suivante\n bash execution.sh\n");
+    return 0;
 }
